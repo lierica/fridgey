b1c57a04822694439c4213d04498aff925e282c1
lierica <ericaaa.li@gmail.com>
2017-12-06 15:44:26 -0500

    added create ingredients

--- app/controllers/fridge_ingredients_controller.rb
+++ app/controllers/fridge_ingredients_controller.rb
@@ -8,7 +8,12 @@ class FridgeIngredientsController < ApplicationController
 
 	def create
 		@fridge = Fridge.find(params[:fridge_id])
-		@fridge_ingredient = FridgeIngredient.create(ingredient_id: params[:fridge_ingredient][:ingredient_id], fridge_id: params[:fridge_id], date_in: params[:fridge_ingredient][:date_in])
-		redirect_to fridge_path(@fridge)
+		if params[:name].empty?
+			@fridge_ingredient = FridgeIngredient.create(ingredient_id: params[:fridge_ingredient][:ingredient_id], fridge_id: params[:fridge_id], date_in: params[:fridge_ingredient][:date_in])
+		else
+			@ingredient = Ingredient.create(name: params[:name], expiration_days: params[:expiration_days])
+			@fridge_ingredient = FridgeIngredient.create(ingredient_id: @ingredient.id, fridge_id: params[:fridge_id], date_in: params[:fridge_ingredient][:date_in])
+		end
+			redirect_to fridge_path(@fridge)
 	end
 end
